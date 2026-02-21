6e92578423b4bd6cf7c9d51a75051a21b3aa27e9
masroof-maindak <mujtaba.asim.amin@gmail.com>
2026-02-21 15:53:36 +0500

    feat: vec3f iterator

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,77 @@
 use std::{fs::File, io::Write, path::Path};
 
-struct Point3D;
+#[derive(Clone)]
+struct Vec3f {
+    r: f32,
+    b: f32,
+    g: f32,
+}
 
-// Saves the output image as a .ppm file
-fn save_image(
-    fpath: &Path,
-    width: usize,
-    height: usize,
-    body: Vec<Point3D>,
-) -> std::io::Result<()> {
-    let mut file = File::create(fpath)?;
+impl IntoIterator for Vec3f {
+    type Item = f32;
+    type IntoIter = Vec3fIterator;
 
-    let header = format!("P6\n{width} {height}\n255\n").into_bytes();
-    file.write(&header)?;
+    fn into_iter(self) -> Self::IntoIter {
+        Vec3fIterator { pt: self, idx: 0 }
+    }
+}
 
-    // TODO: write body.
-    for _px in body {}
+struct Vec3fIterator {
+    pt: Vec3f,
+    idx: usize,
+}
 
-    Ok(())
+impl Iterator for Vec3fIterator {
+    type Item = f32;
+    fn next(&mut self) -> Option<Self::Item> {
+        let ret = match self.idx {
+            0 => Some(self.pt.r),
+            1 => Some(self.pt.g),
+            2 => Some(self.pt.b),
+            _ => None,
+        };
+
+        self.idx += 1;
+        ret
+    }
 }
 
 fn main() -> std::io::Result<()> {
     println!("Hello, world!");
 
-    save_image(Path::new("Out.ppm"), 1920, 1080)?;
+    let fpath = "Out.ppm";
+    // save_image(Path::new("Out.ppm"), 1920, 1080)?;
+
+    let mut file = File::create(fpath)?;
+
+    let width = 1920;
+    let height = 1080;
+    let mut framebuffer: Vec<Vec3f> = vec![
+        Vec3f {
+            r: 0.0,
+            b: 0.0,
+            g: 0.0
+        };
+        width * height
+    ];
+
+    // Populate framebuffer w/ gradient
+    for i in 0..=height {
+        for j in 0..=width {
+            framebuffer[i + j * width] = Vec3f {
+                r: j as f32 / height as f32,
+                b: 0.0,
+                g: i as f32 / width as f32,
+            }
+        }
+    }
+
+    // Save framebuffer to file
+    let header = format!("P6\n{width} {height}\n255\n").into_bytes();
+    file.write(&header)?;
+    for px in framebuffer {
+        for x in px {}
+    }
 
     Ok(())
 }
